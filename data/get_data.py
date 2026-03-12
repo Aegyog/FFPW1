d45ebf6a524185cc23efe02d6120f6949f2413eb
Justin <errolpriniel@gmail.com>
2026-03-13 01:55:33 +0800

    Update get_data.py

--- data/get_data.py
+++ data/get_data.py
@@ -1,1 +1,5 @@
+# Placeholder for Week 1
+# This script will be updated in Week 2 to automatically download and extract 
+# the Microsoft News Dataset (MIND) following the Microsoft Research License.
 
+print("MIND Dataset download pipeline to be implemented.")
